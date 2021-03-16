21c4e87cf6b0e7ce8996ef442ffe4ad3fff7b0ef
pwho <patrick@patrickwho.me>
2021-03-16 11:08:19 -0400

    Format test file as per new prettier config

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,14 +1,14 @@
-const { assert } = require("chai");
+const { assert } = require('chai')
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    
-    await greeter.deployed();
-    assert(await greeter.greet() === "Hello, world!")
+describe('Greeter', function () {
+    it("Should return the new greeting once it's changed", async function () {
+        const Greeter = await ethers.getContractFactory('Greeter')
+        const greeter = await Greeter.deploy('Hello, world!')
 
-    await greeter.setGreeting("Hola, mundo!");
-    assert(await greeter.greet() === "Hola, mundo!")
-  });
-});
+        await greeter.deployed()
+        assert((await greeter.greet()) === 'Hello, world!')
+
+        await greeter.setGreeting('Hola, mundo!')
+        assert((await greeter.greet()) === 'Hola, mundo!')
+    })
+})
